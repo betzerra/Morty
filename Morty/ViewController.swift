9354e9db63e6ae6fd5dbde9cb54b95459f0d9b90
Ezequiel Becerra <ezequiel.becerra@gmail.com>
2021-05-27 00:38:50 -0300

    Initial commit

--- Morty/ViewController.swift
+++ Morty/ViewController.swift
@@ -6,21 +6,53 @@
 //
 
 import Cocoa
+import EventKit
 
 class ViewController: NSViewController {
-
+    var store = EKEventStore()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // Request access to reminders.
+        store.requestAccess(to: .event) { granted, error in
+            // Handle the response to the request.
+            print("Hello world")
+        }
+    }
+    
+    func fetchEvents() -> [EKEvent] {
+        guard let yesterday = dateByAdding(days: -1),
+              let tomorrow = dateByAdding(days: 1) else {
+            
+            return []
+        }
+        
+        let predicate = store.predicateForEvents(
+            withStart: yesterday,
+            end: tomorrow,
+            calendars: nil
+        )
+        
+        return store.events(matching: predicate)
     }
 
+    @IBAction func fetchEvents(_ sender: Any) {
+        let events = fetchEvents()
+        print(events)
+    }
+    
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
     }
+}
 
-
+fileprivate func dateByAdding(days: Int) -> Date? {
+    var components = DateComponents()
+    components.day = days
+    
+    return Calendar.current.date(byAdding: components, to: Date())
 }
 
