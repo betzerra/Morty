47c662b95e7d0fd071c9f0aa2001803d6f143182
Ezequiel Becerra <ezequiel.becerra@gmail.com>
2021-05-27 00:01:56 -0300

    Initial Commit

---
+++ Morty/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Morty
+//
+//  Created by Ezequiel Becerra on 27/05/2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
